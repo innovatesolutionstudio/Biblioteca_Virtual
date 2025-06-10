e144badaf84a4b60264f977087f43f56fb6d8875
karcito <adeikar699@gmail.com>
2025-06-10 01:22:05 -0400

    Version Oficial

--- src/routes/pagina/admin.js
+++
@@ -1,5 +0,0 @@
-const { ensureAuthenticated } = require('../../middlewares/auth');
-router.get('/Subir-Dataset', ensureAuthenticated, (req, res) => {
-  const flaskHost = process.env.FLASK_HOST;
-  res.render('Evolucion/subir_dataset', { flaskHost });
-});
